3464379bbd068e2e2820b0ca7baf6ff24f42d0ad
volochayev <stingray8888831@gmail.com>
2025-08-01 16:05:55 +0300

    add App, SB, movieService, MovieGrid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,2 +1,46 @@
 import { useState } from 'react'
-import './App.css'
+import SearchBar from '../SearchBar/SearchBar'
+import MovieGrid from '../MovieGrid/MovieGrid'
+import toast, { Toaster } from 'react-hot-toast'
+import { type Movie } from '../../types/movie'
+import { fetchMovies } from '../../services/movieService'
+import css from "./App.module.css"
+
+export default function App() {
+    const [hasError, setHasError] = useState<boolean>(false)
+    const [movies, setMovies] = useState<Movie[]>([])
+    const [currentMovie, setCurrentMovie] = useState<Movie | null>(null)
+    const selectMovie = (movie: Movie) => {
+        setCurrentMovie(movie)
+    }
+
+    const searchSubmit = async (query: string) => {
+        
+        setHasError(false)
+        setMovies([])
+
+        try {
+            const results = await fetchMovies(query)
+            if (results.length === 0) {
+                toast.error("No movies found for your request.")
+                return
+            }
+            setMovies(results)
+            
+        } catch {
+            setHasError(true)
+        }
+    }
+    return (
+    <div className={css.app}>
+        <Toaster />
+            <SearchBar onSubmit={searchSubmit} />
+            <MovieGrid onSelect={selectMovie} movies={movies}/>
+    </div>
+)
+};
+
+
+
+
+
---
+++ src/services/movieService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { type Movie } from "../types/movie"; 
+
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const myKey = import.meta.env.VITE_API_KEY;
+
+interface FetchMovies {
+    results: Movie[]
+}
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+    const configObj = {
+        params: { query },
+  headers: {
+    Authorization: `Bearer ${myKey}`
+  }
+    }
+    const response = await axios.get<FetchMovies>(BASE_URL, configObj);
+    return response.data.results;
+}
